938937330947cb2733e57e5c0de82d725e5accaf
Guru Prakash Ram <guruprakashram99@gmail.com>
2021-01-20 17:23:30 +0530

    Add ingrdients to shopping list on click

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from './../recipe.model';
+import { ShoppingListService } from './../../shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +9,11 @@ import { Recipe } from './../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+  addToShoppingList() {
+    for (let ingredient of this.recipe.ingredients) {
+      this.shoppingListService.addIngredients(ingredient);
+    }
+  }
 }
